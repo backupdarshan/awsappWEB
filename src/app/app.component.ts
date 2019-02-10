be604961125e1aaccd02c079306f194756e0814c
backupdarshan <47488346+backupdarshan@users.noreply.github.com>
2019-02-10 06:06:30 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css','./app.component2.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+
+
+  noofbuckets=12;
+
+
+
+  
+}
